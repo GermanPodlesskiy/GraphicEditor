a85cd2eff28acf27ee1f2d6d3c8d515436cba46d
Подлесский Герман Валерьевич <podlesskiy99@mail.ru>
2019-01-18 16:19:18 +0300

    Сhoice of color and thickness.

--- GraphicEditor/MainWindow.xaml.cs
+++ GraphicEditor/MainWindow.xaml.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using GraphicEditor.Shapes;
 using Ellipse = GraphicEditor.Shapes.Ellipse;
+using Figure = GraphicEditor.Shapes.Figure;
 using Line = GraphicEditor.Shapes.Line;
+using MessageBox = System.Windows.MessageBox;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 using Rectangle = GraphicEditor.Shapes.Rectangle;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
 namespace GraphicEditor
 {
@@ -21,12 +28,14 @@ namespace GraphicEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double x1, x2, y1, y2, thickness = 2;
+        private double x1, x2, y1, y2;
         private ushort tag;
         private const string  FILE_FITLER = "*.jpeg|*.jpeg";
         private List<UIElement> deleteFigure = new List<UIElement>(); 
         private Dictionary<int,Figure> figures = new Dictionary<int, Figure>();
         private Brush color = Brushes.Blue;
+        private bool draw;
+        private bool oneFigure;
 
         public MainWindow()
         {
@@ -44,13 +53,13 @@ namespace GraphicEditor
 
         private void ChangingObjects()
         {
-            figures.Add(0, new Line(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(1, new Circle(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(2, new Ellipse(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(3, new Rectangle(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(4, new Square(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(5, new Triangle(new Point(x1, y1), new Point(x2, y2), color, thickness));
-            figures.Add(6, new RightTriangle(new Point(x1, y1), new Point(x2, y2), color, thickness));
+            figures.Add(0, new Line(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(1, new Circle(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(2, new Ellipse(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(3, new Rectangle(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(4, new Square(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(5, new Triangle(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
+            figures.Add(6, new RightTriangle(new Point(x1, y1), new Point(x2, y2), color, sliderThickness.Value));
         }
         
         
@@ -62,6 +71,8 @@ namespace GraphicEditor
             y1 = point.Y;
             labelX.Content = "X = " + x1;
             labelY.Content = "Y = " + y1;
+            draw = true;
+            oneFigure = false;
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
@@ -88,27 +99,60 @@ namespace GraphicEditor
             }
         }
 
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw && (e.LeftButton == MouseButtonState.Pressed))
+            {
+                Point point = e.GetPosition(canvas);
+                x2 = point.X;
+                y2 = point.Y;
+                ChangingObjects();
+                if (oneFigure)
+                    canvas.Children.RemoveAt(canvas.Children.Count - 1);
+                figures[tag].Draw(canvas);
+                oneFigure = true;
+
+                figures.Clear();
+            }
+        }
+
+        private void ColorPicture_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                color = new SolidColorBrush(Color.FromArgb(dlg.Color.A,dlg.Color.R,dlg.Color.G,dlg.Color.B));
+            }
+        }
+
+        private byte[] BitmapSourceToByteArray(BitmapSource bmpSrc)
+        {
+            var encoder = new JpegBitmapEncoder();
+            encoder.QualityLevel = 100;
+            encoder.Frames.Add(BitmapFrame.Create(bmpSrc));
+
+            using (var stream = new MemoryStream())
+            {
+                encoder.Save(stream);
+                return stream.ToArray();
+            }
+        }
+
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             Point point = e.GetPosition(canvas);
             x2 = point.X;
             y2 = point.Y;
             ChangingObjects();
-
             figures[tag].Draw(canvas);
-            
             figures.Clear();
+            draw = false;
         }
 
         private void Picture_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Image image = sender as Image;
             tag = Convert.ToUInt16(image.Tag);
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
         }
 
         private void SaveAs_Click(object sender, RoutedEventArgs e)
